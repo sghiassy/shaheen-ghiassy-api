c337fb4cdc3afd116fe9edffed512ce5dbaeafa9
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2017-02-19 14:28:38 -0800

    Adding form logic

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,16 +1,47 @@
 'use strict';
 
-module.exports.incoming_request = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'You have contacted Shaheen Ghiassy\'s API',
-      input: event,
-    }),
-  };
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const qs = require('qs');
+const Entities = require('html-entities').XmlEntities;
+const entities = new Entities();
+const conf = require('../../config/form.json');
 
-  callback(null, response);
+module.exports.submit = (event, context, callback) => {
+  const functionArnCols = context.invokedFunctionArn.split(':');
+  const region = functionArnCols[3];
+  const accountId = functionArnCols[4];
+  const topicArn = 'arn:aws:sns:' + region + ':' + accountId + ':' + topicName;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const done = (err, result) => callback(null, {
+    statusCode: err ? '500' : '200',
+    body: JSON.stringify({'status':err ? 'bad': 'roger'}),
+    headers: {
+        "Access-Control-Allow-Origin" : "http://shaheenghiassy.com", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
+  });
+
+  var obj = qs.parse(event.body);
+  var message = "";
+  var validMessage = true;
+
+  formKeys.forEach(function(val, i) {
+    if (obj[val] === undefined) {
+      validMessage = false;
+    }
+    message += val.toUpperCase() + "\n" + entities.decode("" + obj[val]) + "\n\n";
+  });
+
+  message += "\n\n-----\n\n";
+
+  if (validMessage) {
+    sns.publish({
+      Message: message,
+      Subject: MAIL_TITLE,
+      TopicArn: topicArn
+    }, done);
+  } else {
+    done();
+  }
 };
